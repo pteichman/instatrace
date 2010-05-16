fde3cb61225f66414f41eac141fb68651a3fe1c9
Peter Teichman <peter@teichman.org>
2010-05-15 22:25:42 -0400

    Implement testStatistics with count, total, mean, and stddev checks

--- tests/stats.py
+++ tests/stats.py
@@ -2,11 +2,24 @@
 
 import unittest
 
-from instatrace.stats import Statistic
+from instatrace.stats import Statistics
 
-class testStatistic(unittest.TestCase):
-    def testName(self):
+class testStatistics(unittest.TestCase):
+    def testStats(self):
         name = "test_stat"
-        s = Statistic(name, [])
+        s = Statistics()
 
-        self.assertEqual(name, s.name)
+        s.add_sample(name, 1)
+        s.add_sample(name, 2)
+        s.add_sample(name, 3)
+
+        test_stat = s.statistics.get(name)
+
+        self.assertEqual(name, test_stat._name)
+
+        stats = test_stat.stats()
+
+        self.assertEqual(3, stats.get("count"))
+        self.assertEqual(6, stats.get("total"))
+        self.assertAlmostEqual(2, stats.get("mean"))
+        self.assertAlmostEqual(0.8164966, stats.get("stddev"))
