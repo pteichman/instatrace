72ba75d4e2e976d30792ebd4b83b43eb4e743e7a
Peter Teichman <peter@teichman.org>
2010-05-15 22:41:30 -0400

    Implement testStandardDeviation

--- tests/stats.py
+++ tests/stats.py
@@ -23,3 +23,25 @@ class testStatistics(unittest.TestCase):
         self.assertEqual(6, stats.get("total"))
         self.assertAlmostEqual(2, stats.get("mean"))
         self.assertAlmostEqual(0.8164966, stats.get("stddev"))
+
+    def testStandardDeviation(self):
+        name = "test_stat"
+
+        s = Statistics()
+
+        s.add_sample(name, 2)
+        s.add_sample(name, 4)
+        s.add_sample(name, 4)
+        s.add_sample(name, 4)
+        s.add_sample(name, 5)
+        s.add_sample(name, 5)
+        s.add_sample(name, 7)
+        s.add_sample(name, 9)
+
+        test_stat = s.statistics.get(name)
+
+        self.assertEqual(name, test_stat._name)
+
+        stats = test_stat.stats()
+
+        self.assertAlmostEqual(2, stats.get("stddev"))
